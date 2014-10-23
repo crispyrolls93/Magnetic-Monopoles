472158783715355974463d161c96054399e5f18e
Nathan Cosbie-Ross <ncosbie.ross1@gmail.com>
2014-10-23 02:30:20 +0100

    Update Mag_Mon.f90

--- Mag_Mon.f90
+++ Mag_Mon.f90
@@ -11,57 +11,57 @@ PROGRAM Mag_Mon
 
   !Defines variables as real but with twice the decimal places for
   !optimal accuracy
-  DOUBLE PRECISION :: m, qa, qg, rlim, c, dt, vc
+  DOUBLE PRECISION :: q, dt, vix, viy, n, b, bx, by, m
   DOUBLE PRECISION :: x, y, fx, fy, vx, vy, rx, ry, r, v, ri
   
   !All constants neccesary for the formulae are defined below 
-  qa = 2
-  qg = 79
-  c = 137.035999
-  m = 7294.3
+  q = 1
+  m = 9.10938291 * 10 **(-31)
   dt = 1.0 * 10.0 ** (-5.0)
   y = -0.005
 
   !The intial velocity and x displacement are requested from the user
-  WRITE(*,*) 'Please enter the initial horizontal displacement'
-  WRITE(*,*) 'of the alpha particle'
+  WRITE(*,*) 'Please enter the initial x displacement'
+  WRITE(*,*) 'of the electron'
   READ(*,*) x
-  WRITE(*,*) 'Please enter the initial velocity in terms of c'
-  WRITE(*,*) 'of the alpha particle'
-  READ(*,*) vc
-
-  !The velocity is read in from the equation in terms of c so this converts
-  !to atomic units
-  v = vc * c
+  WRITE(*,*) 'Please enter the initial y displacement'
+  WRITE(*,*) 'of the electron'
+  READ(*,*) y
+  WRITE(*,*) 'Please enter the initial x velocity'
+  WRITE(*,*) 'of the electron'
+  READ(*,*) vix
+  WRITE(*,*) 'Please enter the initial y velocity'
+  WRITE(*,*) 'of the electron'
+  READ(*,*) viy
+  WRITE(*,*) 'Please enter the strength of the monopole'
+  READ(*,*) b
 
   !Defines inital velocities and positions in both directions
-  vx = 0
-  vy = v
+  vx = vix
+  vy = viy
   rx = x
   ry = y
 
   !Calculates the initial value of the total separation
   ri = SQRT((x ** 2) + (y ** 2))
 
-  !Provides the limiting factor
-  rlim = ABS(1.1 * ri)
-  
-  !Initialise the r variable so that rlim > r
-  r = 0
+  !Initialises the count n
+  n = 0
 
   !Opens the output file
-  OPEN(1, FILE='assign_11_1.out')
+  OPEN(1, FILE='Mag_Mon.out')
 
   !Writes the initial x and y displacement to the output file
   WRITE(1,*) x, y
 
   !Repeats the iterations of the formulae within the loop till r > rlim
-  DO WHILE (rlim .GE. r)
+  DO WHILE (n .LE. 1000)
 
-    !Formulae using coulombs equation to get the force at a given time and 
+    !Formulae using lorentz equation to get the force at a given time and 
     !the the change in velocity and position of the electron
-    fx = (rx / SQRT(rx ** 2 + ry ** 2)) * ((qa * qg) / (rx ** 2 + ry ** 2))
-    fy = (ry / SQRT(rx ** 2 + ry ** 2)) * ((qa * qg) / (rx ** 2 + ry ** 2))
+    n = n + 1
+    fx = (q * vx * bx)
+    fy = (q * vy * by)
     vx = vx + (fx / m) * dt
     vy = vy + (fy / m) * dt
     rx = rx + vx * dt

4d8addd8a439e24951ab4381b981f215b248c0ee
Nathan Cosbie-Ross <ncosbie.ross1@gmail.com>
2014-11-06 15:01:35 +0000

    Update Mag_Mon.f90

--- Mag_Mon.f90
+++ Mag_Mon.f90
@@ -1,7 +1,7 @@
 !Program written by Nathan Cosbie-Ross Last edited 06/11/14
-!Program designed to  to simulate an electron in a 2d electron gas
+!Program designed to simulate an electron in a 2d electron gas
 !interacting above an artificial magnetic monopole.
-!Allows input of varying initial velocities and displacements
+!Allows input of varying initial velocities, displacements and monopole strengths
 
 PROGRAM Mag_Mon
 
@@ -11,19 +11,20 @@ PROGRAM Mag_Mon
   !optimal accuracy
   !q = charge of electron, dt = time difference, n = count
   !m = mass of electron, h = height of plane above monopole
-  !b = magnetic field strength, r = separation, rp = separation in plane
-  !rc = separation from projection, f = force, v = velocity
-  !theta = angle
+  !b = magnetic field strength, r = separation, rc = separation in plane
+  !rp = separation from projection, f = force, v = velocity
+  !theta = angle around plane, phi = angle perpendicular to plane
   
-  DOUBLE PRECISION :: q, dt, n, m, h, b, bx, by, bi, btot, theta, phi
-  DOUBLE PRECISION :: r, ri, rx, ry, rc, rp, rtot, fx, fy, vx, vy, v
+  DOUBLE PRECISION :: q, dt, n, m, h, b, bx, by, bc, btot, theta, phi
+  DOUBLE PRECISION :: r, rx, ry, rc, rp, rtot, fx, fy, vx, vy
   
   !All constants neccesary for the formulae are defined below 
-  q = 1
+  q = 1.602 * 10 ** (-19)
   m = 9.10938291 * 10 **(-31)
   dt = 1.0 * 10.0 ** (-5.0)
 
-  !The intial velocity and x displacement are requested from the user
+  !The intial velocities, displacements and magnetic strength
+  !are requested from the user
   WRITE(*,*) 'Please enter the initial x displacement'
   WRITE(*,*) 'of the electron'
   READ(*,*) rx
@@ -54,31 +55,38 @@ PROGRAM Mag_Mon
   !Repeats the iterations of the formulae within the loop till n = 1000
   DO WHILE (n .LE. 1000)
 
-    !Formulae using lorentz equation to get the force at a given time and 
-    !the the change in velocity and position of the electron
-    n = n + 1
-    fx = (q * vx * bx)
-    fy = (q * vy * by)
+    !Calculates separation from projection of monopole
+    rc = SQRT((rx ** 2) + (ry ** 2))
+    !Calculates total separation from monopole
+    rtot = SQRT((rc ** 2) + (rp ** 2))
+    
+    !Calculates angle counter-clockwise from projection of monopole in x axis
+    theta = atan(ry / rx)
+    !Calculates angle from monopole perpendicular to x-y plane
+    phi = atan(rc / rp)
+    
+    !Calculates magnetic field strength at point and then in x and y respectively
+    bc = btot * sin(phi)
+    bx = bc * cos(theta)
+    by = bc * sin(theta)
+
+    !Formulae using lorentz equation to get the force at a given time 
+    fx = (q * vy * by)
+    fy = (q * vx * bx)
+
+    !Adds the change in velocity due to the velocity
     vx = vx + (fx / m) * dt
     vy = vy + (fy / m) * dt
+
+    !Adds to the displacement due to velocity
     rx = rx + vx * dt
     ry = ry + vy * dt
     r = SQRT((rx ** 2) + (ry ** 2) + (h ** 2))
 
-   !Calculates separation from projection of monopole
-    rc = SQRT((rx ** 2) + (ry ** 2))
-    !Calculates angle counter-clockwise from projection of monopole in x axis
-    theta = asin(ry / rc)
-    !Calculates total separation from monopole
-    rtot = SQRT((rc ** 2) + (rp ** 2))
-    !Calculates angle from monopole perpendicular to x-y plane
-    phi = asin(rc / rtot)
-    !Calculates magnetic field strength at point and then in x and y respectively
-    bi = rtot * sin(phi)
-    bx = bi * cos(theta)
-    by = bi * sin(theta)
+    !Adds to the count
+    n = n + 1
     
-     !Outputs the displacement in terms of x and y to the output file
+    !Outputs the displacement in terms of x and y to the output file
     WRITE(1,*) rx, ry 
 
   !Ends the loop

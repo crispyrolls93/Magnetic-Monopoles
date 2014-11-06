41a465522b1270722b865c94ecd29a4974cb835b
Nathan Cosbie-Ross <ncosbie.ross1@gmail.com>
2014-11-06 02:20:01 +0000

    Update Mag_Mon.f90

--- Mag_Mon.f90
+++ Mag_Mon.f90
@@ -15,45 +15,37 @@ PROGRAM Mag_Mon
   !optimal accuracy
   !q = charge of electron, dt = time difference, n = count
   !m = mass of electron, h = height of plane above monopole
-  !b = magnetic field strength, r = separation
+  !b = magnetic field strength, r = separation, rp = separation in plane
   !rc = separation from projection, f = force, v = velocity
+  !theta = angle
   
-  DOUBLE PRECISION :: q, dt, n, m, h, b, bx, by, bxi, byi, bi
-  DOUBLE PRECISION :: r, ri, rxi, ryi, rx, ry, rc, fx, fy, vx, vy, v, vxi, vyi
+  DOUBLE PRECISION :: q, dt, n, m, h, b, bx, by, bxi, byi, bi, btot, theta, phi
+  DOUBLE PRECISION :: r, ri, rxi, ryi, rx, ry, rc, rp, rtot, fx, fy, vx, vy, v, vxi, vyi
   
   !All constants neccesary for the formulae are defined below 
   q = 1
   m = 9.10938291 * 10 **(-31)
   dt = 1.0 * 10.0 ** (-5.0)
-  y = -0.005
 
   !The intial velocity and x displacement are requested from the user
   WRITE(*,*) 'Please enter the initial x displacement'
   WRITE(*,*) 'of the electron'
-  READ(*,*) x
+  READ(*,*) rx
   WRITE(*,*) 'Please enter the initial y displacement'
   WRITE(*,*) 'of the electron'
-  READ(*,*) y
+  READ(*,*) ry
   WRITE(*,*) 'Please enter the initial x velocity'
   WRITE(*,*) 'of the electron'
-  READ(*,*) vxi
+  READ(*,*) vx
   WRITE(*,*) 'Please enter the initial y velocity'
   WRITE(*,*) 'of the electron'
-  READ(*,*) vyi
+  READ(*,*) vy
   WRITE(*,*) 'Please enter the strength of the monopole'
-  READ(*,*) bi
-
-  !Defines inital velocities and positions in both directions
-  vx = vxi
-  vy = vyi
-  rx = x
-  ry = y
-  bxi = 
-  byi = 
-
-  !Calculates the initial value of the total separation
-  ri = SQRT((x ** 2) + (y ** 2))
-
+  READ(*,*) btot
+  WRITE(*,*) 'Please enter the separtaion of the monopole'
+  WRITE(*,*) 'from the plane'
+  READ(*,*) rp
+  
   !Initialises the count n
   n = 0
 
@@ -63,7 +55,7 @@ PROGRAM Mag_Mon
   !Writes the initial x and y displacement to the output file
   WRITE(1,*) x, y
 
-  !Repeats the iterations of the formulae within the loop till r > rlim
+  !Repeats the iterations of the formulae within the loop till n = 1000
   DO WHILE (n .LE. 1000)
 
     !Formulae using lorentz equation to get the force at a given time and 
@@ -75,10 +67,22 @@ PROGRAM Mag_Mon
     vy = vy + (fy / m) * dt
     rx = rx + vx * dt
     ry = ry + vy * dt
-    rc = SQRT((rx ** 2) + (ry ** 2))
     r = SQRT((rx ** 2) + (ry ** 2) + (h ** 2))
- 
-    !Outputs the displacement in terms of x and y to the output file
+
+   !Calculates separation from projection of monopole
+    rc = SQRT((rx ** 2) + (ry ** 2))
+    !Calculates angle counter-clockwise from projection of monopole in x axis
+    theta = asin(ry / rc)
+    !Calculates total separation from monopole
+    rtot = SQRT((rc ** 2) + (rp ** 2))
+    !Calculates angle from monopole perpendicular to x-y plane
+    phi = asin(rc / rtot)
+    !Calculates magnetic field strength at point and then in x and y respectively
+    bi = rtot * sin(phi)
+    bx = bi * cos(theta)
+    by = bi * sin(theta)
+    
+     !Outputs the displacement in terms of x and y to the output file
     WRITE(1,*) rx, ry 
 
   !Ends the loop

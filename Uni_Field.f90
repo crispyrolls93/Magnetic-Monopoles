b5cde58495b7a0eec599f34521c4de8125f4daa7
Nathan Cosbie-Ross <ncosbie.ross1@gmail.com>
2014-11-12 22:34:22 +0000

    Update Uni_Field.f90

--- Uni_Field.f90
+++ Uni_Field.f90
@@ -1,80 +1,70 @@
 !Program written by Nathan Cosbie-Ross Last edited 06/11/14
 !Program designed to simulate an electron in a 2d electron gas
-!interacting with a uniform magnetic field
-!Allows input of varying initial velocities, displacements and monopole strengths
+!interacting in a uniform magnetic field.
+!Allows input of varying initial velocities and displacements
 
-PROGRAM Mag_Mon
+PROGRAM Uni_Field
 
   IMPLICIT NONE
 
   !Defines variables as real but with twice the decimal places for
   !optimal accuracy
   !q = charge of electron, dt = time difference, n = count
-  !m = mass of electron, h = height of plane above monopole
+  !m = mass of electron
   !b = magnetic field strength, r = separation, rc = separation in plane
-  !rp = separation from projection, f = force, v = velocity
-  !theta = angle around plane, phi = angle perpendicular to plane
+  !f = force, v = velocity !theta = angle around plane
   
-  DOUBLE PRECISION :: qe, qm, dt, n, m, h, b, bx, by, bc, bp, pi
-  DOUBLE PRECISION :: r, rx, ry, rc, rp, fx, fy, vx, vy, mu0, ep0
+  DOUBLE PRECISION :: qe, dt, n, m, h, b, d, pi
+  DOUBLE PRECISION :: rx, ry, rc, fx, fy, vx, vy
   
   !All constants necessary for the formulae are defined below 
   qe = 1.602 * 10 ** (-19)
   m = 9.10938291 * 10 **(-31)
-  dt = 1.0 * 10.0 ** (-5.0)
-  mu0 = 1.25663706 * 10 ** (-6)
   pi = 3.14159265359
 
   !The initial velocities, displacements and magnetic strength
   !are requested from the user
   WRITE(*,*) 'Please enter the initial x displacement'
-  WRITE(*,*) 'of the electron'
+  WRITE(*,*) 'of the electron (nm)'
   READ(*,*) rx
   WRITE(*,*) 'Please enter the initial y displacement'
-  WRITE(*,*) 'of the electron'
+  WRITE(*,*) 'of the electron (nm)'
   READ(*,*) ry
   WRITE(*,*) 'Please enter the initial x velocity'
-  WRITE(*,*) 'of the electron'
+  WRITE(*,*) 'of the electron (nm/s)'
   READ(*,*) vx
   WRITE(*,*) 'Please enter the initial y velocity'
-  WRITE(*,*) 'of the electron'
+  WRITE(*,*) 'of the electron (nm/s)'
   READ(*,*) vy
-  WRITE(*,*) 'Please enter the strength of the monopole'
-  READ(*,*) qm
-  WRITE(*,*) 'Please enter the separation of the monopole'
-  WRITE(*,*) 'from the plane'
-  READ(*,*) rp
-  
+  WRITE(*,*) 'Please enter the strength of the field (nT)'
+  READ(*,*) b
+  WRITE(*,*) 'Please enter the timestep (ns)'
+  READ(*,*) dt
+    
   !Initialises the count n
   n = 0
 
   !Opens the output file
-  OPEN(1, FILE='Mag_Mon.out')
+  OPEN(1, FILE='Uni_Field.out')
 
   !Writes the initial x and y displacement to the output file
   WRITE(1,*) rx, ry
-
+  WRITE(1,*) qe, vx, vy
   !Repeats the iterations of the formulae within the loop till n = 1000
-  DO WHILE (n .LE. 1000)
+  DO WHILE (n .LE. 10)
 
-    !Calculates separation from projection of monopole
-    rc = SQRT((rx ** 2) + (ry ** 2))
-    
-    !Calculates magnetic field strength at point perpendicular to the plane.
-    bp = (mu0 / 4 * pi) * ((qm * rp) / (((rp ** 2) + (rc ** 2)) ** (3 / 2)))
-    
+        
     !Formulae using lorentz equation to get the force at a given time 
-    fx = (qe * vy * bp)
-    fy = (qe * vx * bp)
-
+    fx = (qe * vy * b)
+    fy = (qe * vx * b)
+	WRITE(1,*) fx, fy 
     !Adds the change in velocity due to the velocity
     vx = vx + (fx / m) * dt
     vy = vy + (fy / m) * dt
-
+	WRITE(1,*) vx, vy
     !Adds to the displacement due to velocity
     rx = rx + vx * dt
     ry = ry + vy * dt
-    r = SQRT((rx ** 2) + (ry ** 2) + (h ** 2))
 
     !Adds to the count
     n = n + 1

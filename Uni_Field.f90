5fd3d99e3e0d95c422560795e13ddedf0ce92976
Nathan Cosbie-Ross <ncosbie.ross1@gmail.com>
2014-11-12 23:42:01 +0000

    Update Uni_Field.f90

--- Uni_Field.f90
+++ Uni_Field.f90
@@ -14,12 +14,12 @@ PROGRAM Uni_Field
   !b = magnetic field strength, r = separation, rc = separation in plane
   !f = force, v = velocity !theta = angle around plane
   
-  DOUBLE PRECISION :: qe, dt, n, m, h, b, d, pi
+  DOUBLE PRECISION :: qe, dt, n, m, h, b, d, pi, nmax
   DOUBLE PRECISION :: rx, ry, rc, fx, fy, vx, vy
   
   !All constants necessary for the formulae are defined below 
-  qe = 1.602 * 10 ** (-19)
-  m = 9.10938291 * 10 **(-31)
+  qe = 1.602 * 10 ** (-19.0)
+  m = 9.10938291 * 10 **(-31.0)
   pi = 3.14159265359
 
   !The initial velocities, displacements and magnetic strength
@@ -40,7 +40,8 @@ PROGRAM Uni_Field
   READ(*,*) b
   WRITE(*,*) 'Please enter the timestep (ns)'
   READ(*,*) dt
-    
+  WRITE(*,*) 'Please enter the number of iterations'
+  READ(*,*) nmax
   !Initialises the count n
   n = 0
 
@@ -49,15 +50,17 @@ PROGRAM Uni_Field
 
   !Writes the initial x and y displacement to the output file
   WRITE(1,*) rx, ry
-  WRITE(1,*) qe, vx, vy
+  
+  !Closes the output file
+  CLOSE(1)
   !Repeats the iterations of the formulae within the loop till n = 1000
-  DO WHILE (n .LE. 10)
-
+  DO WHILE (n .LE. nmax)
+    OPEN(1, file='Uni_Field.out', POSITION='append')
         
     !Formulae using lorentz equation to get the force at a given time 
-    fx = (qe * vy * b)
+    fx = -(qe * vy * b)
     fy = (qe * vx * b)
-	WRITE(1,*) fx, fy 
+	  WRITE(1,*) fx, fy
     !Adds the change in velocity due to the velocity
     vx = vx + (fx / m) * dt
     vy = vy + (fy / m) * dt
@@ -71,12 +74,11 @@ PROGRAM Uni_Field
     
     !Outputs the displacement in terms of x and y to the output file
     WRITE(1,*) rx, ry 
-
+    CLOSE(1)
   !Ends the loop
   END DO
 
-  !Closes the output file
-  CLOSE(1)
+
 
 !Ends the program
 END PROGRAM

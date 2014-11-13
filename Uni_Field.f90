747d640b7f95831f973674faf3f04b34a6b834e6
Nathan Cosbie-Ross <ncosbie.ross1@gmail.com>
2014-11-13 00:32:39 +0000

    Update Uni_Field.f90

--- Uni_Field.f90
+++ Uni_Field.f90
@@ -42,28 +42,28 @@ PROGRAM Uni_Field
   READ(*,*) dt
   WRITE(*,*) 'Please enter the number of iterations'
   READ(*,*) nmax
+  !Converts Magnetic field and displacements/velocities
+  b = b * 10 ** (-9.0)
+  rx = rx * 10 ** (-9.0)
+  ry = ry * 10 ** (-9.0)
+  vx = vx * 10 ** (-9.0)
+  vy = vy * 10 ** (-9.0)
   !Initialises the count n
   n = 0
 
   !Opens the output file
   OPEN(1, FILE='Uni_Field.out')
-
-  !Writes the initial x and y displacement to the output file
-  WRITE(1,*) rx, ry
-  
-  !Closes the output file
-  CLOSE(1)
+    
   !Repeats the iterations of the formulae within the loop till n = 1000
   DO WHILE (n .LE. nmax)
-    OPEN(1, file='Uni_Field.out', POSITION='append')
-        
+   
     !Formulae using lorentz equation to get the force at a given time 
     fx = -(qe * vy * b)
     fy = (qe * vx * b)
-	  WRITE(1,*) fx, fy
+	WRITE(1,*) fx, fy
     !Adds the change in velocity due to the velocity
-    vx = vx + (fx / m) * dt
-    vy = vy + (fy / m) * dt
+    vx = vx + (fx / m) * dt * 10 ** (-9.0)
+    vy = vy + (fy / m) * dt * 10 ** (-9.0)
 	WRITE(1,*) vx, vy
     !Adds to the displacement due to velocity
     rx = rx + vx * dt
@@ -74,11 +74,12 @@ PROGRAM Uni_Field
     
     !Outputs the displacement in terms of x and y to the output file
     WRITE(1,*) rx, ry 
-    CLOSE(1)
+  
   !Ends the loop
   END DO
-
-
+  WRITE(*,*) 'Iterations Complete'
+  !Closes the output file
+  CLOSE(1)
 
 !Ends the program
 END PROGRAM

8be3627414b88a89ecbd4a46bb171d5f697fb6b3
Nathan Cosbie-Ross <ncosbie.ross1@gmail.com>
2015-02-21 14:35:24 +0000

    Update Uni_Field.f90

--- Uni_Field.f90
+++ Uni_Field.f90
@@ -1,87 +1,70 @@
-!Program written by Nathan Cosbie-Ross Last edited 06/11/14
-!Program designed to simulate an electron in a 2d electron gas
-!interacting in a uniform magnetic field.
-!Allows input of varying initial velocities and displacements
+!Program Written by Nathan Cosbie-Ross Last Edited 21/02/15
+!Program Designed to Simulate an Electron in a 2D Electron Gas Interacting in a Uniform Magnetic Field.
 
+!-----Begins Program-----!
 PROGRAM Uni_Field
-
+  
+  !-----Fix For Legacy Flaw-----!
   IMPLICIT NONE
-
-  !Defines variables as real but with twice the decimal places for
-  !optimal accuracy
-  !q = charge of electron, dt = time difference, n = count
-  !m = mass of electron
+  
+  !-----Defines All Variable Types-----!
+  !q = charge of electron, dt = time difference, n = count, m = mass of electron
   !b = magnetic field strength, r = separation, rc = separation in plane
   !f = force, v = velocity !theta = angle around plane
+  DOUBLE PRECISION :: qe, dt, n, m, h, b, d, i, imax, tmax, t
+  DOUBLE PRECISION :: rx, ry, rc, fx, fy, vx, vy , mu
   
-  DOUBLE PRECISION :: qe, dt, n, m, h, b, d, pi, nmax
-  DOUBLE PRECISION :: rx, ry, rc, fx, fy, vx, vy
+  !-----All Constants Necessary For The Formulae Are Defined-----!
+  qe = 1
+  m = 1
   
-  !All constants necessary for the formulae are defined below 
-  qe = 1.602 * 10 ** (-19.0)
-  m = 9.10938291 * 10 **(-31.0)
-  pi = 3.14159265359
-
-  !The initial velocities, displacements and magnetic strength
-  !are requested from the user
-  WRITE(*,*) 'Please enter the initial x displacement'
-  WRITE(*,*) 'of the electron (nm)'
-  READ(*,*) rx
-  WRITE(*,*) 'Please enter the initial y displacement'
-  WRITE(*,*) 'of the electron (nm)'
-  READ(*,*) ry
-  WRITE(*,*) 'Please enter the initial x velocity'
-  WRITE(*,*) 'of the electron (nm/s)'
-  READ(*,*) vx
-  WRITE(*,*) 'Please enter the initial y velocity'
-  WRITE(*,*) 'of the electron (nm/s)'
-  READ(*,*) vy
-  WRITE(*,*) 'Please enter the strength of the field (nT)'
-  READ(*,*) b
-  WRITE(*,*) 'Please enter the timestep (ns)'
-  READ(*,*) dt
-  WRITE(*,*) 'Please enter the number of iterations'
-  READ(*,*) nmax
-  !Converts Magnetic field and displacements/velocities
-  b = b * 10.0 ** (-9.0)
-  rx = rx * 10.0 ** (-9.0)
-  ry = ry * 10.0 ** (-9.0)
-  vx = vx * 10.0 ** (-9.0)
-  vy = vy * 10.0 ** (-9.0)
-  dt = dt * 10.0 ** (-9.0)
-  !Initialises the count n
+  !-----Initial Conditions Defined-----!
+  rx = 1
+  ry = 0
+  vx = 0
+  vy = 1
+  b = 1
+  dt = 0.001
+  imax = 10000
+  
+  !-----Initialise Count-----!
   n = 0
-
+  i = 0
+  
   !Opens the output file
-  OPEN(1, FILE='Uni_Field.out')
-    
+  OPEN(1, FILE='Uni_Field.dat')
+  
   !Repeats the iterations of the formulae within the loop till n = 1000
-  DO WHILE (n .LE. nmax)
-   
-    !Formulae using lorentz equation to get the force at a given time 
+  DO WHILE (i .LE. imax)
+
+	!-----Iterates Lorentz Force-----!
     fx = (qe * vy * b)
     fy = -(qe * vx * b)
-	
-    !Adds the change in velocity due to the velocity
+
     vx = vx + (fx / m) * dt
     vy = vy + (fy / m) * dt
-	
-    !Adds to the displacement due to velocity
+    
     rx = rx + vx * dt
     ry = ry + vy * dt
-
-    !Adds to the count
-    n = n + 1
     
-    !Outputs the displacement in terms of x and y to the output file
-    WRITE(1,*) rx, ry 
+	!-----Adds To The Count-----!
+    i = i + 1 
+	
+	!-----Updates Terminal With Percentage Completion-----!
+	IF (t >= (n/10) * tmax) THEN
+		WRITE(*,*) n * 10, '% Complete'
+		n = n + 1
+	END IF
+
+    
+	!-----Writes To Output File-----!
+    WRITE(1,*) rx, ry   
   
-  !Ends the loop
+  !-----Ends Do Loop-----!
   END DO
-  WRITE(*,*) 'Iterations Complete'
-  !Closes the output file
+  
+  !-----Closes Output File-----!
   CLOSE(1)
-
-!Ends the program
+  
+!-----Ends Program-----!
 END PROGRAM
-
